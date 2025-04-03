0350453464dd8097189044c129de18b421752d92
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-04-03 16:09:48 +0200

    codigo

--- Ejercicio_1_/base_ej_1.py
+++ Ejercicio_1_/base_ej_1.py
@@ -1,6 +1,12 @@
+import sys
+import os
+
+# Agregar el directorio raíz al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from calculo_torre_hanoi import torre_hanoi
 
 def ejercicio1():
-    n = 74
+    n = 5
     total_combinaciones = torre_hanoi(n, 'A', 'C', 'B')
     print("Total de combinaciones:", total_combinaciones)
