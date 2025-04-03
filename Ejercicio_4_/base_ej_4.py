da17ad90959ea0ccc1e0e5a956e6019b01eec20d
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-04-03 18:09:46 +0200

    codigo ejercicio 4

--- Ejercicio_4_/base_ej_4.py
+++ Ejercicio_4_/base_ej_4.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Agregar el directorio raíz al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from funciones import restar, dividir, eliminar_termino, existe_termino
 
 class Polinomio:
