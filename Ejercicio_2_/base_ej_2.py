ce0d61c549a88e7e53463bce3e0d6bbac28da979
ardillaCHIKI <svegazos@student.universidadunie.com>
2025-04-03 16:53:33 +0200

    codigo ejercicio 3

--- Ejercicio_2_/base_ej_2.py
+++ Ejercicio_2_/base_ej_2.py
@@ -1,5 +1,10 @@
+import sys
+import os
+
+# Agregar el directorio raíz al PYTHONPATH
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from Metodos import determinante_recursivo, determinante_iterativo
-#from Metodos import determinante_iterativo
 
 # Ejemplo de uso
 def ejercicio2():
